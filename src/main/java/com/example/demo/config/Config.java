dd7a955ddfcc385f33090f6b264541ecd0151297
sepehr <sepehrgh01@gmail.com>
2020-08-14 11:47:39 +0430

    Removed redis

--- src/main/java/com/example/demo/config/Config.java
+++ src/main/java/com/example/demo/config/Config.java
@@ -4,21 +4,13 @@ import labs.psychogen.row.annotations.EnableRowPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-import org.springframework.security.authentication.AuthenticationTrustResolver;
-import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.savedrequest.NullRequestCache;
-import org.springframework.session.data.redis.RedisFlushMode;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
-import org.springframework.session.web.http.HttpSessionIdResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
@@ -42,7 +34,6 @@ public class Config {
     @EnableGlobalMethodSecurity(prePostEnabled = true)
     @Configuration
     @EnableAspectJAutoProxy
-    @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 5 * 24 * 60 * 60, redisFlushMode = RedisFlushMode.ON_SAVE)
     static class MyWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
@@ -62,24 +53,5 @@ public class Config {
                 .csrf().disable()
                 .rememberMe().alwaysRemember(true);
         }
-    }
-
-    @Bean
-    public LettuceConnectionFactory lettuceConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setDatabase(0);
-        redisStandaloneConfiguration.setHostName("localhost");
-        return new LettuceConnectionFactory(redisStandaloneConfiguration);
-    }
-
-
-    @Bean
-    public static AuthenticationTrustResolver getAuthenticationTrustResolver() {
-        return new AuthenticationTrustResolverImpl();
-    }
-
-    @Bean
-    public HttpSessionIdResolver httpSessionIdResolver() {
-        return HeaderHttpSessionIdResolver.xAuthToken();
     }
 }
